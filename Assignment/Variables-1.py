dcdff49911bc535415d22078e71f4cfd07ac9f4a
Apurbo Kabbo <apurbokabbo1999@gmail.com>
2026-04-22 17:46:14 +0600

    variables added

---
+++ Assignment/Variables-1.py
@@ -0,0 +1,10 @@
+city,zipcode,area,age,price,list,boolean = "Dhaka", "1216" , "Mirpur", 27, 127.27,[1,2,3,4,5,6,"One", "Two"], True
+print(city)
+print(zipcode)
+print(area)
+print(age)
+print(price)
+print(list)
+print(boolean)
+
+
---
+++ Assignment/Variables-2.py
@@ -0,0 +1,3 @@
+p= 3.1416
+radius =5
+print(f"Area of Circle Radius is {radius} = " , p*radius*radius)
