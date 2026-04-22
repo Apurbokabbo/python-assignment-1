846caadbd31251283defe7482d8609c97974a194
Apurbo Kabbo <apurbokabbo1999@gmail.com>
2026-04-22 18:27:07 +0600

    operators adding

---
+++ Assignment/Operators/Operators-1.py
@@ -0,0 +1,8 @@
+a = 50
+b = 26
+
+print("Addition :", a + b)
+print("Subtraction :", a - b)
+print("Multiplication :", a * b)
+print("Division :", a / b)
+print("Modulo :", a % b)
---
+++ Assignment/Operators/Operators-2.py
@@ -0,0 +1,9 @@
+num_1 = int(input("Enter a number: "))
+num_2 = int(input("Enter another number: "))
+
+if num_1 > num_2:
+    print(f"The number one {num_1} is greater than the number two {num_2}")
+elif num_1 < num_2:
+    print(f"The number one {num_1} is less than the number two {num_2}")
+else:
+    print(f"The number one {num_1} and two {num_2} are equal")
