f9f54ad883d02bc54e61eb1d8009253ff4ffe15f
Apurbo Kabbo <apurbokabbo1999@gmail.com>
2026-04-23 18:28:01 +0600

    type-casting added

---
+++ Assignment/Type-Casting/Type-Casting-1.py
@@ -0,0 +1,4 @@
+price = 127.4343
+converted_price = int(price)
+
+print(f"Actual Value: {price}, Converted Value: {converted_price}")
---
+++ Assignment/Type-Casting/Type-Casting-2.py
@@ -0,0 +1,3 @@
+value = str(input("Enter  number: "))
+converted_value = int(value)+10
+print(f"Enter Value : {value} Converted Value: {converted_value}")
